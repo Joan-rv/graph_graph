6357a0512d26149203028d72718e9c0c62a24f37
Joan Ripoll <Joan-rv@users.noreply.github.com>
2025-03-14 20:51:53 +0100

    Try keeping node distance uniform

--- src/graph_draw.c
+++ src/graph_draw.c
@@ -4,7 +4,9 @@
 #include <time.h>
 
 #define EDGE_LEN 100.0f
-#define SPRING_STRENGTH 1.0f
+#define NODE_DIST 100.0f
+#define EDGE_STRENGTH 1.0f
+#define NODE_STRENGTH 1.0f
 
 Vector2 *initialize_positions(struct graph_s graph, int width, int height) {
     Vector2 *node_positions =
@@ -43,9 +45,24 @@ void update_graph_positions(struct graph_s graph, Vector2 *positions,
             float dist = Vector2Distance(pos1, pos2);
             float delta_dist = dist - EDGE_LEN;
             Vector2 move_normal = Vector2Normalize(Vector2Subtract(pos2, pos1));
-            float move_scale = dt * delta_dist * SPRING_STRENGTH;
+            float move_scale = dt * delta_dist * EDGE_STRENGTH;
             positions[i] =
                 Vector2Add(pos1, Vector2Scale(move_normal, move_scale));
+        }
+    }
+
+    for (size_t i = 0; i < graph.nodes_size; i++) {
+        for (size_t j = i + 1; j < graph.nodes_size; j++) {
+            Vector2 pos1 = positions[i];
+            Vector2 pos2 = positions[j];
+            float dist = Vector2Distance(pos1, pos2);
+            float delta_dist = dist - NODE_DIST;
+            Vector2 move_normal = Vector2Normalize(Vector2Subtract(pos2, pos1));
+            float move_scale = dt * delta_dist * NODE_STRENGTH;
+            positions[i] =
+                Vector2Add(pos1, Vector2Scale(move_normal, move_scale));
+            positions[j] =
+                Vector2Add(pos2, Vector2Scale(move_normal, -move_scale));
         }
     }
 }
